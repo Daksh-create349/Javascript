38b61dea3b11da76db311b809faaf2e5b032d9f0
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-05 13:43:18 +0530

    Day 1 of Javascript

--- First.js
+++ First.js
@@ -1,8 +1,18 @@
+//All Console Functions
 console.log("This is the First.js file.");
 console.info("First.js is executing.");
 console.error("This is an error message from First.js.");
 console.warn("This is a warning from First.js.");
 console.debug("Debugging information from First.js.");
+
+//Variable Basic
 a = 2;
 b = 3;
 console.log(a + b);
+
+//let func and backticks
+let fname = 'Daksh'; 
+let lname = 'Srivastava';
+let mname = `Kumar`;
+let greet = ` "Hello" ${fname}`; 
+console.log(greet);
