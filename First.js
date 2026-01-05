ee1320ca16255e2bc9d602d5a53d056a8d8e5ec3
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-05 13:29:45 +0530

    Day 1 of Javascript

---
+++ First.js
@@ -0,0 +1,8 @@
+console.log("This is the First.js file.");
+console.info("First.js is executing.");
+console.error("This is an error message from First.js.");
+console.warn("This is a warning from First.js.");
+console.debug("Debugging information from First.js.");
+a = 2;
+b = 3;
+console.log(a + b);
