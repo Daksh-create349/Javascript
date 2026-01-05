988f4664b942cdab7d9bd87327542591cc049ddb
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-05 14:08:50 +0530

    Day 1 Homework of Javascript

---
+++ Activity.js
@@ -0,0 +1,29 @@
+//Task 1
+console.log("Hello! I'm your coding fun fact guide.");
+
+//Task 2
+let Botname = "CodeBot";
+let BotLocation = "Cyberspace";
+let FavaouriteLanguage = "JavaScript";
+
+//Task 3
+console.log(`My name is ${Botname}. I reside in ${BotLocation} .`);
+
+//Task 4
+console.log(`My favourite programming language is ${FavaouriteLanguage}.`);
+
+//Task 5
+let codingfact = `Did you know? ${FavaouriteLanguage} was created in just 10 days by Brendan Eich in 1995!`;
+
+
+//Task 6
+console.log(codingfact);
+
+//Task 7
+codingfact = `Fav language is ${FavaouriteLanguage}, Easiest language`;
+
+//Task 8
+console.log(codingfact);
+
+//Task 10
+console.log(`Goodbye from CodeBot! - ${Botname},${BotLocation}.`);
