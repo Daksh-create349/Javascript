e2bbe81236405c5cc3e5e3f1890c05864d5b6cd9
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-06 16:31:32 +0530

    Day 2 of Javascript

---
+++ Activity2.js
@@ -0,0 +1,72 @@
+let challenge = "50 days of javascript";
+
+//Task 2
+console.log(challenge);
+
+//Task 3
+console.log(challenge.length);
+
+//Task 4
+console.log(challenge.toUpperCase());
+
+//Task 5
+console.log(challenge.toLowerCase());
+
+//Task 6
+console.log(challenge.substring(0, 3));
+
+//Task 7
+console.log(challenge.substring(3));
+
+//Task 8
+console.log(challenge.includes("Script"));
+
+//Task 9
+console.log(challenge.split(" "));
+
+//Task 10
+console.log(challenge.split(""));
+
+//Task 11
+let dreamcompanies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon, LetsUpgrade";
+console.log(dreamcompanies.split(", "));
+
+//Task 12
+console.log(challenge.replace("javascript", "Python"));
+
+//Task 13
+console.log(challenge.charAt(15));
+
+//Task 14
+console.log(challenge.charCodeAt(11));
+
+//Task 15
+console.log(challenge.lastIndexOf("a"));
+
+//16.
+let phrase = "You cannot end a sentence with because because because is a conjunction";
+console.log(phrase.indexOf("because"));
+//17.
+console.log(phrase.lastIndexOf("because"));
+//18.
+console.log(phrase.search("because"));
+
+//19.
+console.log(challenge.trim());
+
+//20.   
+console.log(challenge.startsWith("50"));
+
+//21.
+console.log(challenge.endsWith("t"));
+
+//22.
+console.log(challenge.match(/a/g));
+
+//23.
+let string = "30 Days of JavaScript";
+console.log(string.concat(" - Welcome to the world of JavaScript"));
+
+//24.
+console.log(challenge.repeat(2));
+
---
+++ Script.js
@@ -0,0 +1,10 @@
+
+let placement = null;
+console.log(placement);
+
+let fname = "Daksh";
+fname[0] = "d";
+console.log(fname);
+
+let inUpperCase = fname.toUpperCase();
+console.log(inUpperCase);
