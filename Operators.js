0f652cb7fd9effe42ac373e2197a0ec6194beedf
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-07 10:41:51 +0530

    Day 3 of Javascript

---
+++ Operators.js
@@ -0,0 +1,44 @@
+//what is operand?
+//Operand is the value on which operator operates
+
+//what is operator?
+//Operator is a symbol that tells the compiler or interpreter to perform specific mathematical, relational or logical operation and produce final result
+
+//binary plus
+//Binary plus operator adds two operands
+
+//unary plus
+//Unary plus operator converts its operand into a number
+
+//increment
+//Unary increment operator increments (adds one to) its operand
+
+//decrement
+//Unary decrement operator decrements (subtracts one from) its operand
+
+
+let a= 68;
+let b=1;
+console.log(a+b)
+
+console.log(typeof "10" == 10);
+console.log(typeof "10" === 10);
+fname="10";
++fname;
+Number(fname);
+console.log(fname);
+
+//++var_name //--var_name will increment first and then use the value
+//var_name++ //var_name-- will use the value first and then increment
+
+let counter = 1;
+counter++
+console.log(counter);
+
+let count = 1;
+
+console.log(++count);
+
+
+
+
