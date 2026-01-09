c4393db3788cb88ff00b6979ddba26e0c0adc68e
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-09 11:05:51 +0530

    Day 5 of Javascript

--- loops.js
+++ loops.js
@@ -1,5 +1,31 @@
+// For loop
 for (let i = 1; i < 5; i++) {
 
-    alert(`The number is ${i}`);
+    console.log(`The number is ${i}`);
     if (i == 2) continue;
 }
+
+
+
+let lname = "Daksh";
+let output = "";
+
+for (let i = 0; i < lname.length; i++) {
+    output += i + " ";
+}
+
+console.log((output));
+
+// While loop
+let n = 1;
+while (n < 6) {
+    console.log(n);
+    n++;
+}
+ 
+// Do while loop
+
+do {
+    console.log(n);
+    n++;
+} while (n < 6);
