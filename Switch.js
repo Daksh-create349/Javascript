723080dd0075ba2dfe57a39e73574090c1b7b4c4
Daksh Srivastava <dakshshrivastav56@gmail.com>
2026-01-09 10:13:30 +0530

    Day 4 of Javascript

---
+++ Switch.js
@@ -0,0 +1,13 @@
+let age = 15;
+
+switch(true){
+    case age > 15:
+        console.log(`You are above 15`);
+
+        case age === 15:
+            console.log(`You are 15 years old`);
+        break;
+
+        default:
+            console.log(`Enter valid age`);
+}
---
+++ loops.js
@@ -0,0 +1,5 @@
+for (let i = 1; i < 5; i++) {
+
+    alert(`The number is ${i}`);
+    if (i == 2) continue;
+}
